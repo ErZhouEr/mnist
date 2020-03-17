4a7c2168239741d77f6603db5c4c50dc9c557bc3
ErZhouEr <917846003@qq.com>
2020-03-17 22:56:42 +0800

    convolution完成

--- cnn/convolution.py
+++ cnn/convolution.py
@@ -3,7 +3,7 @@
 
 """
 @author:l
-@file: convolution_nn.py
+@file: convolution.py
 @time: 2020/03/05
 """
 
@@ -83,14 +83,15 @@ class ConvLayer(object):
 		# 这个计算原理：补0后的sensitivity_map要能够作为输入得到真正输入array的width
 		# 即解方程：(sen_map_width - filter_width + 2*pad)/stride+1 = input_width
 		padded_map = util_funcs.pad(sensitivity_map, pad)
-		# 3 针对每个filter，进行误差传递
+		# 3 针对每个filter，进行误差传递, 传递的结果是得到与上一层（即input）相同维度的误差项
 		self.delta_array = np.zeros_like(self.input_array)  # 卷积层的误差项，input_array的形状：[channel,height,width]
 		# 多个filter，需要将每个filter对应的误差项相加
 		for f in range(self.filter_num):
 			fliped_filter = np.rot90(self.filters[f].get_weights(), 2)
 			tmp = np.zeros((self.input_height, self.input_width))
-			# 多个channel，需要将filter的channel与对应input的channel对应
+			# 多个channel，需要将filter的channel与上一层的channel对应
 			for d in tmp.shape[0]:
+				# filter 的个数对应着输出的层数，所以padded_map[f]
 				util_funcs.convolution(padded_map[f], fliped_filter[d], 1, tmp[d], 0)
 			self.delta_array += tmp
 			# 最后得到的delta是维度[channel,height,width]的array
@@ -104,10 +105,20 @@ class ConvLayer(object):
 	def bp_gradient(self, sensitivity_array):
 		'''
 		反向传播计算梯度
-		:param sensitivity_array:
-		:return:
+		:param sensitivity_array: 本层误差项
 		'''
-		pass
+		# 1 卷积步长不一定为1，对于不为1的，要对原始sensitivity map进行扩展
+		expanded_map=self.expand_sensitivity_map(sensitivity_array)
+		# 2 计算 w 的梯度，本层误差项与本层输入（上层输出）进行卷积，结果得到与filter维度相同的梯度array
+		for f in range(self.filter_num):
+			filter=self.filters[f]
+			for d in range(filter.channel):
+				# 每个不同channel的误差项是一样的，想想前向时候输出是怎么计算的就理解了
+				# 为什么用padded_input_arr？因为padded_input_arr与filter卷积得到输出的维度，
+				# padded_input_arr与输出卷积得到filter的维度
+				util_funcs.convolution(self.padded_input_arr[d],expanded_map[f],1,filter.weight_grads[d],0)
+			# 每个filter一个bias
+			filter.bias_grad=expanded_map[f].sum()
 
 	def expand_sensitivity_map(self, sensitivity_array):
 		'''
